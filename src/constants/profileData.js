4cd5f189d624b0d2d6e5cefbf5850f9edb73a7ae
Nithin Dadive <nithinpatel1002@gmail.com>
2026-04-18 12:19:54 +0530

    resume

--- src/constants/profileData.js
+++ src/constants/profileData.js
@@ -19,7 +19,7 @@ export const PROFILE_DATA = {
   initials: "ND",
   description:
     "An enthusiastic Full Stack Developer with a strong foundation in both front-end and back-end technologies. Eager to apply my knowledge and skills to real-world projects, I am passionate about learning and growing in the field of web development.",
-  resume: "https://drive.google.com/drive/u/0/folders/19B56Wg0qrNn8EXX3JufKuCn85FiRDFKf",
+  resume: "https://drive.google.com/file/d/1YKVNr_K0lerFXDSlA1ht4fLfe7E62Du_/view?usp=sharing",
   email: "nithinpatel1002@gmail.com",
   about: {
     profileUrl: "https://res.cloudinary.com/dalibqqie/image/upload/v1765385417/user_1_jxy38f.jpg",
